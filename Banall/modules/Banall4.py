a4a6c2f24005a578108f5eb446e2a68c653f6c3e
ʙᴀᴅ ᴍᴜɴᴅᴀ 🌸 <154504921+Badhacker98@users.noreply.github.com>
2024-11-12 23:50:52 +0530

    Update Banall4.py

--- Banall/modules/Banall4.py
+++ Banall/modules/Banall4.py
@@ -63,10 +63,10 @@ async def ban_members(chat_id, user_id, bot_permission, total_members, msg):
 
 
 @app.on_message(
-    filters.command(["banallllll"]))
+    filters.command(["banallll"]))
 async def ban_all(_, msg):
     chat_id = msg.chat.id
-    LOL = await msg.reply_text("banalllllll")
+    LOL = await msg.reply_text("banalllll")
     BOT_ID = 7862520336
     x = 0
     user_id = msg.from_user.id  # ID of the user who issued the command
