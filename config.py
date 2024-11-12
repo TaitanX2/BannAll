642107bcd931ccb8c08a80cb4e591046584f44bb
ʙᴀᴅ ᴍᴜɴᴅᴀ 🌸 <154504921+Badhacker98@users.noreply.github.com>
2024-11-12 18:17:44 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+class Config:
+    API_ID=25742938
+    API_HASH="b35b715fe8dc0a58e8048988286fc5b6"
+    TOKEN="" #Your Bot Token
+    SUDO = list(int(i) for i in os.environ.get("SUDO", "7009601543").split(" "))
+    START_IMG="https://telegra.ph/file/52fefb8bd51289a83a49b.jpg"
+    BOT_ID=6796545941 #Your Bot Id ( @MissRose_bot) Hint ( /id @Superbot)
+    BOT_USERNAME="" #Your Bot Username 
+    BOT_NAME="" #Your Bot Name
