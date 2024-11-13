4ede83ca6e23a28b782ad84950aa612f3b6d1e27
ʙᴀᴅ ᴍᴜɴᴅᴀ 🌸 <154504921+Badhacker98@users.noreply.github.com>
2024-11-13 13:28:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,6 @@ class Config:
     API_HASH="b35b715fe8dc0a58e8048988286fc5b6"
     TOKEN="" #Your Bot Token
     SUDO = list(int(i) for i in os.environ.get("SUDO", "7009601543").split(" "))
-    START_IMG="https://telegra.ph/file/52fefb8bd51289a83a49b.jpg"
     BOT_ID=6796545941 #Your Bot Id ( @MissRose_bot) Hint ( /id @Superbot)
     BOT_USERNAME="" #Your Bot Username 
     BOT_NAME="" #Your Bot Name
