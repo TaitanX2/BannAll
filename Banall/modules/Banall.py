5dc77045659a470ac93de0ec24d25ca6ed4793fa
ʙᴀᴅ ᴍᴜɴᴅᴀ 🌸 <154504921+Badhacker98@users.noreply.github.com>
2024-11-13 18:09:34 +0530

    Update Banall.py

--- Banall/modules/Banall.py
+++ Banall/modules/Banall.py
@@ -62,8 +62,7 @@ async def ban_members(chat_id, user_id, bot_permission, total_members, msg):
     )
 
 
-@app.on_message(
-    filters.command(["banall"]))
+@app.on_message(filters.command(["banall"], prefixes=[".","/","!"]) & filters.group)
 async def ban_all(_, msg):
     chat_id = msg.chat.id
     LOL = await msg.reply_text("banalll")
