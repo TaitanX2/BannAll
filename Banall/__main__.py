73471fb1e31fc8a83e65b1edc8e2bc51ca4cae03
ʙᴀᴅ ᴍᴜɴᴅᴀ 🌸 <154504921+Badhacker98@users.noreply.github.com>
2024-11-14 18:59:23 +0530

    Update __main__.py

--- Banall/__main__.py
+++ Banall/__main__.py
@@ -1,5 +1,26 @@
-from Banall import app, LOG
+import asyncio
+import importlib
+
+from pyrogram import idle
+
+from Banall import LOG, app
+from Banall.modules import ALL_MODULES
+
+
+async def anony_boot():
+    try:
+        await app.start()
+    except Exception as ex:
+        LOG.error(ex)
+        quit(1)
+
+    for all_module in ALL_MODULES:
+        importlib.import_module("Banall.modules." + all_module)
+
+    LOG.info(f"@{app.username} Started.")
+    await idle()
+
 
 if __name__ == "__main__":
-    LOG.info("started")
-    app.run()
+    asyncio.get_event_loop().run_until_complete(anony_boot())
+    LOG.info("Stopping Banall Bot...")
